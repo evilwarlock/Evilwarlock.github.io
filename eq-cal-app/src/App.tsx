ea62565b8ef3a79ea23e6e0b79555c6279c4e719
Evilwarlock <ceaxyz@gmail.com>
2023-12-23 14:37:53 -0800

    update ap

--- eq-cal-app/src/App.tsx
+++ eq-cal-app/src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [handOne, setHandOne] = useState('');
+  const [handTwo, setHandTwo] = useState('');
+  const [result, setResult] = useState('');
+
+  const calculateWinner = () => {
+    // Logic to calculate winner based on poker hands
+    // This is a placeholder - you'd replace this with real logic
+    if (handOne && handTwo) {
+      setResult('Winner calculated based on hands!');
+    } else {
+      setResult('Please enter both hands to calculate winner.');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Poker Hand Calculator</h1>
+        <div>
+          <label>
+            Hand 1:
+            <input
+              type="text"
+              value={handOne}
+              onChange={(e) => setHandOne(e.target.value)}
+              placeholder="Enter Hand 1"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Hand 2:
+            <input
+              type="text"
+              value={handTwo}
+              onChange={(e) => setHandTwo(e.target.value)}
+              placeholder="Enter Hand 2"
+            />
+          </label>
+        </div>
+        <button onClick={calculateWinner}>Calculate Winner</button>
+        {result && <p>{result}</p>}
       </header>
     </div>
   );
